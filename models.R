ae3cab11b400c04a133d6a281fe1b4825542c6df
jkrrrr <102986583+jkrrrr@users.noreply.github.com>
2023-10-18 09:26:16 +0100

    added seperate contributor models

---
+++ models.R
@@ -0,0 +1,25 @@
+# File containing all models - each contributor has their own model
+
+# Function to create a classifier and evaluate
+# inputs:
+# training_data - data frame - the data to train the model on
+# testing_data - data frame - the data to evaluate the model on
+ModelHarry<-function(training_data,testing_data){
+  
+}
+
+ModelChris<-function(training_data,testing_data){
+  
+}
+
+ModelAnna<-function(training_data,testing_data){
+  
+}
+
+ModelMelric<-function(training_data,testing_data){
+  
+}
+
+ModelZion<-function(training_data,testing_data){
+  
+}
